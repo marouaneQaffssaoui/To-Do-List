2e882bfd63461744fa07b71a2435875880caeccf
marouane <marouane.qaffssaoui@uit.ac.ma>
2025-06-20 17:51:51 +0100

    I finalise the app

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { useEffect, useState } from "react";
 import CreateTask from "./components/CreateTask";
 import ListTask from "./components/ListTask";
 import { Toaster } from 'react-hot-toast';
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
+
 
 function App() {
   const [tasks, setTasks] = useState(() => {
@@ -9,14 +12,14 @@ function App() {
     return saved ? JSON.parse(saved) : [];
   });
   return (
-    <>
+    <DndProvider backend={HTML5Backend}>
       <Toaster />
       <div className="bg-slate-300 w-screen h-screen 
-      flex flex-col items-center gap-16 pt-24">
+      flex flex-col items-center p-3 gap-16 pt-24">
         <CreateTask tasks={tasks} setTasks={setTasks}/>
         <ListTask tasks={tasks} setTasks={setTasks}/>
       </div>
-    </>
+    </DndProvider>
     
   )
 }
--- src/components/ListTask.jsx
+++ src/components/ListTask.jsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
+import { useDrag, useDrop } from "react-dnd";
+import toast from "react-hot-toast";
 
 const ListTask = ({ tasks, setTasks }) => {
     const [todos, setTodos] = useState([]);
@@ -14,7 +16,127 @@ const ListTask = ({ tasks, setTasks }) => {
         setClosed(fClosed)
     }, [tasks]);
     const statuses = ["todo", "inprogress", "closed"];
-    return <div> List </div>;
+    return (
+        <div className="flex gap-16">
+            {statuses.map((status, index) =>
+                (<Section key={index} 
+                    status={status} 
+                    tasks={tasks} 
+                    setTasks={setTasks} 
+                    todos={todos} 
+                    inProgress={inProgress} 
+                    closed={closed}
+                />
+            ))}
+        </div>
+    );
 };
 
 export default ListTask;
+
+const Section = ({status, 
+    tasks, 
+    setTasks, 
+    todos, 
+    inProgress, 
+    closed}) => {
+    
+    const [{ isOver}, drop] = useDrop(() => ({
+        accept: "task",
+        drop: (item) => addItemToSection(item.id),
+        collect: (monitor) => ({
+            isOver: !!monitor.isOver()
+        })
+    }))
+    
+    let text = "Todos";
+    let bg = "bg-slate-500";
+    let tasksToMap = todos;
+    if (status === "inprogress") {
+        text = "In Progress";
+        bg = "bg-purple-500";
+        tasksToMap = inProgress;
+    }
+    if (status === "closed") {
+        text = "Closed";
+        bg = "bg-green-500";
+        tasksToMap = closed;
+    }
+
+    const addItemToSection = (id) => {
+        setTasks(prev => {
+            const mTasks = prev.map(t => {
+                if(t.id === id){
+                    return{...t, status: status}
+                }
+                return t;
+            });
+            localStorage.setItem("tasks", JSON.stringify(mTasks));
+            toast("Task status changed", {icon: "😃"});
+            return mTasks;
+        })
+    }
+
+    return(
+        <div ref={drop} className={`w-64 rounded-sm p-2 ${isOver ? "bg-slate-200" : ""}`}>
+            <Header text={text} bg={bg} count={tasksToMap.length} /> 
+            {tasksToMap.length > 0 && tasksToMap.map(task => <Task 
+                key={task.id} task={task} tasks={tasks} setTasks={setTasks}
+            />)}
+        </div>
+    );
+};
+
+const Header = ({text, bg, count}) => {
+    return(
+        <div className={`${bg} flex items-center justify-center h-12 pt-4 rounded-md uppercase text-sm text-white`}>
+            {text}
+            <div className="ml-2 bg-white w-5 h-5 text-black rounded-full flex items-center justify-center">
+                {count}
+            </div>
+        </div>
+    );
+};
+ 
+const Task = ({task, tasks, setTasks}) => {
+
+    const [{ isDragging }, drag] = useDrag(() => ({
+        type: "task",
+        item: {id: task.id},
+        collect: (monitor) => ({
+            isDragging: !!monitor.isDragging()
+        })
+    }))
+
+    const handleRemove = (id) => {
+        const fTasks = tasks.filter(t => t.id !== id)
+        localStorage.setItem("tasks", JSON.stringify(fTasks))
+        setTasks(fTasks);
+
+        toast.error("Task removed");
+    }
+
+    return(
+        <div ref={drag} 
+                className={`relative p-4 mt-8 shadow-md rounded-md cursor-grab 
+                ${isDragging ? "opacity-25" : "opacity-100"}`
+            }>
+            <p>{task.name}</p>
+            <button className="absolute bottom-1 right-1 text-slate-400" 
+                onClick={() => handleRemove(task.id)}>
+                <svg 
+                    xmlns="http://www.w3.org/2000/svg" 
+                    fill="none" 
+                    viewBox="0 0 24 24" 
+                    strokeWidth={1.5} 
+                    stroke="currentColor" 
+                    className="size-6">
+                    <path strokeLinecap="round" 
+                        strokeLinejoin="round" 
+                        d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                </svg>
+
+            </button>
+        </div>
+    );
+};
